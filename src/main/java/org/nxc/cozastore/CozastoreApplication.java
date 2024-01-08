e752c5538d4f565abb506aabce00a88c06707e03
chuongnx <chuongnx.0611@gmail.com>
2024-01-09 01:29:43 +0700

    Initial commit

---
+++ src/main/java/org/nxc/cozastore/CozastoreApplication.java
@@ -0,0 +1,13 @@
+package org.nxc.cozastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozastoreApplication.class, args);
+	}
+
+}
